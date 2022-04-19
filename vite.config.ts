b118626ecc78c2a2bbfc632d3a841a46727dd9b9
Katsumi Nakao <nakao.katsumi.75@gmail.com>
2022-04-19 14:33:48 +0900

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,9 @@ const dist = path.join(__dirname, '..', 'docs', path.basename(__dirname))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  base: '/hello-vite-vue/',
+  plugins: [vue()],
+  build: {
+    outDir: './doc'
+  }
 })
